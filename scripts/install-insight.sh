ce872cf223db8bfe63d47504eca3b457045a580e
justanwar <42809091+justanwar@users.noreply.github.com>
2024-11-16 23:31:11 +0800

    Test personal repo

--- scripts/install-insight.sh
+++ scripts/install-insight.sh
@@ -18,7 +18,7 @@ $lib_name/bin/bitcore-node-zcoin create $node_name
 cd $node_name
 
 node_modules/bitcore-node-zcoin/bin/bitcore-node-zcoin install firoorg/insight-ui-zcoin
-node_modules/bitcore-node-zcoin/bin/bitcore-node-zcoin install firoorg/insight-api-zcoin
+node_modules/bitcore-node-zcoin/bin/bitcore-node-zcoin install justanwar/insight-api-zcoin
 
 find . -name "zcore-lib" -type d -not -path "./node_modules/zcore-lib" | xargs rm -rf
 
